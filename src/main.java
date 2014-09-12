5a1e9e87076a825f4b261dd80e16b46e71aee309
esm_cr_1991@hotmail.com <EmiSancho>
2014-09-11 18:37:29 -0600

    cambios en el main

--- src/main.java
+++ src/main.java
@@ -22,8 +22,17 @@ public class main {
      */
     public static void main(String[] args) 
     {
-
-    WordScore wordScore = new WordScore("edder");
+       //ventana p=new ventana();
+        Scanner dato=new Scanner(System.in);
+        String palabra;
+        System.out.print("Ingrese la palabra a buscar\n");
+        palabra=dato.nextLine();
+    if (l.Lectura(palabra)){
+            WordScore wordScore = new WordScore(palabra);
+            int resultScore = wordScore.CalculateScore();
+            System.out.println( resultScore);
+    }
+    WordScore wordScore = new WordScore("palabra");
     int resultScore = wordScore.CalculateScore();
     System.out.println( resultScore);
     }
