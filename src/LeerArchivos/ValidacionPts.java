0cf770916d83e120e20385451a3c081b117c9ea2
edr2394 <edr2394@gmail.com>
2014-09-14 10:15:24 -0600

    Actualizacion de puntajes

--- src/LeerArchivos/ValidacionPts.java
+++ src/LeerArchivos/ValidacionPts.java
@@ -7,18 +7,18 @@ import wordScore.WordScore;
  */
 public class ValidacionPts
 {
-    public static void validepts ()
-    {
-    Lectura l=new Lectura();
-    Scanner dato=new Scanner(System.in);
-    String palabra;
-    System.out.print("Ingrese la palabra a buscar\n");
-    palabra=dato.nextLine();
-    if (l.Lectura(palabra))
-    {
-            WordScore wordScore = new WordScore(palabra);
-            int resultScore = wordScore.CalculateScore();
-            System.out.println( resultScore);
-    }
-    }
+    //public static void validepts ()
+    //{
+    //Lectura l=new Lectura();
+    //Scanner dato=new Scanner(System.in);
+    //String palabra;
+    //System.out.print("Ingrese la palabra a buscar\n");
+    //palabra=dato.nextLine();
+    //if (l.Lectura(palabra))
+    //{
+    //        WordScore wordScore = new WordScore(palabra);
+    //        int resultScore = wordScore.CalculateScore();
+    //       System.out.println( resultScore);
+    //}
+    //}
 }
