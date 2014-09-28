40570c6776c5d83cf58d68e6007209b98a2f3745
root <santigr17@gmail.com>
2014-09-28 16:36:42 -0600

    seriañ

---
+++ src/Juego/Juego.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+import Fichas.bolsa;
+
+import GUI.random;
+import ListasSimples.ListaSimple;
+import ListasSimples.NodoListaSimple;
+/**
+ *
+ * @author santii
+ */
+public class Juego {
+    int numJugadores=0;
+    ListaSimple Orden=new ListaSimple();
+    Object jugador1=null;
+    Object jugador2=null;
+    Object jugador3=null;
+    Object jugador4=null;
+    int tiempo=0;
+    
+    public Juego(){
+       this.jugador1=null;
+       this.jugador2=null;
+       this.jugador3=null;
+       this.jugador4=null;
+    }  
+    public void guardarOrden(Object jugador1,Object jugador2,Object jugador3, Object jugador4){
+        Orden.agregar(jugador1);
+        Orden.agregar(jugador2);
+        Orden.agregar(jugador3);
+        Orden.agregar(jugador4);
+        
+    }
+    public Object Turno(){
+        NodoListaSimple temp1 = Orden.getcabeza();
+        NodoListaSimple temp2 = Orden.getcabeza();
+        while(temp1.getNext()!=null){
+            temp1=temp1.getNext();
+        }
+        temp1.setNext(temp2);
+        return temp2.getelem();
+    }
+      
+    
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 import Fichas.bolsa;
 import GUI.*;
+import Juego.Juego;
 import LeerArchivos.*;
 import LeerArchivos.ValidacionPts;
 import Matrices.*;
@@ -9,21 +10,31 @@ public class main
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception 
     {
-        Matrelleno matriz2 = new Matrelleno();
-        matriz2.rellenar();
+        
+        
+        //Matrelleno matriz2 = new Matrelleno();
+        //matriz2.rellenar();
         /**Coords punto = new Coords();
         punto.getCoord();
         principal P=new principal();
         P.pintar();
         ValidacionPts val = new ValidacionPts();
+        Juego Principal=new Juego();
         jugador jugador1=new jugador();
         jugador1.rellenarAtril();
+        jugador jugador2=new jugador();
+        jugador jugador3=new jugador();
+        jugador jugador4=new jugador();
+//        Principal.Turno();
         bolsa B=new bolsa();
         B.bolsa();
         B.Sacar(1);
-        B.mostrar();*/  
+        B.mostrar();
+        jugador1.mostrar();
+        jugador1.borrarAtril();*/
+        
 
     }
 }
