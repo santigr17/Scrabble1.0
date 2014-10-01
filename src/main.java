57075c1eb9d4a1c1fe47cccbe3f0b09fc25f9e5a
root <santigr17@gmail.com>
2014-10-01 00:38:00 -0600

    santi

--- src/main.java
+++ src/main.java
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
+
 import GUI.*;
-
-public class main 
-{
-
-=======
 import Fichas.Logicbolsa;
 import Fichas.bolsa;
-
-
-
 import GUI.*;
 import Juego.Juego;
-
-
-
 import GUI.*;
 import Juego.Juego;
 
@@ -35,7 +24,7 @@ public class main
      * @param args the command line arguments
      */
 
->>>>>>> 911bd9444521bb0bb8c023f4b7e32b06f6c40522
+
     public static void main(String[] args) 
     {
         VentanaPrincipal Ventana= new VentanaPrincipal();
